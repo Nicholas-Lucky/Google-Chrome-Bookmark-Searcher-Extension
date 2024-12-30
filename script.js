113027ffb5d9a65581ff327590646ceb40bcd52e
Nicholas Lucky <148924180+Nicholas-Lucky@users.noreply.github.com>
2024-12-29 21:57:37 -0600

    Update script.js
    Making a brief definitely not finalized plan for implementing the bookmark searching process

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Event listener for the search bar (not sure if this would happen if the user enters or if there is ANY input yk)
+
+// 2. Google's bookmarks are stored in a tree, gotta traverse through all bookmarks
+
+// 3. While traversing through bookmarks, check if the... name?? is included in the search bar (idk about keywords yet)
+
+// 4. If the name is in the search bar, add it to the results text somehow idk
+
+// 5. Somehow allow this process to occur continuously in case the user searches diff things (rn assuming that this happens automatically)
